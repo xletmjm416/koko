002040ed365828e482d80f20f56f0478d80b3c47
mjm416 <mjm416@ic.ac.uk>
2020-02-13 21:16:13 +0100

    Add AbstractModel class

--- core.py
+++ core.py
@@ -3,3 +3,21 @@
 """
 :author: Mikolaj Metelski
 """
+from abc import ABC, abstractmethod
+
+class AbstractModel(ABC):
+    
+    @abstractmethod
+    def __init__(self, *params):
+        pass
+    
+    @abstractmethod
+    def __call__(self, *data):
+        pass
+
+if __name__ == "__main__":
+    # manual test; put into a unit test
+    try:
+        model = AbstractModel()
+    except TypeError:
+        print("ok")
