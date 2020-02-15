07b7b3242d4f48a567b999e079782a9a504a5829
mjm416 <mjm416@ic.ac.uk>
2020-02-15 03:22:23 +0100

    Change author doscrings

--- core.py
+++ core.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-:author: Mikolaj Metelski
+Author:
+    Mikolaj Metelski
 """
 from abc import ABC, abstractmethod
 
--- testing.py
+++ testing.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-:author: Mikolaj Metelski
+Author:
+    Mikolaj Metelski
 """
 from core import AbstractModel
 from analytics import \
