ed06dba0e74c192e79197e9fcedfeed67d05550b
mjm416 <mjm416@ic.ac.uk>
2020-02-16 14:20:59 +0100

    Update version in __init__

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 """
 :author: Mikolaj Metelski
 """
-__version__ = 0
+__version__ = "0.1"
 __all__ = ["core.AbstractModel",
            "analytics.run_on_param_grid",
            "analytics.calibrate_on_param_grid",
